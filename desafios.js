d258ee0b53284124b393be03acf1b214bcd91f43
Paola Chaves <paochavess22@gmail.com>
2024-03-08 18:54:28 -0500

    desafios

--- desafios.js
+++ desafios.js
@@ -1,2 +1,16 @@
-"User strict"
-let mivariable ="este es un texto de ejemplo"
+"Use strict";
+const entrada = pront("Ingrese su nombre");
+console.log (entrada);
+switch(entrada){
+    case "":
+        document.write("Hola!");
+        break
+     default:
+    document.write("Hola "+ entrada);
+    break;
+}
+
+
+
+
+
