3141cd7496bfd80b9ff548418e01576681ed3334
Paola Chaves <paochavess22@gmail.com>
2024-03-08 22:00:44 -0500

    desafios

--- desafios.js
+++ desafios.js
@@ -1,5 +1,7 @@
 "Use strict";
-const entrada = pront("Ingrese su nombre");
+
+//desafio 1 saludar con nombre
+/*const entrada = prompt("Ingrese su nombre");
 console.log (entrada);
 switch(entrada){
     case "":
@@ -8,9 +10,23 @@ switch(entrada){
      default:
     document.write("Hola "+ entrada);
     break;
-}
+}*/
 
+//desafio 2 pedir numero y sumarlos
+/*let numero1 = prompt("ingrese un numero");
+console.log (numero1);
+let numero2 = prompt("ingrese otro numero");
+console.log (numero2);
+numero1 =parseInt(numero1);
+numero2 =parseInt(numero2);
+resultado = numero1 + numero2;
+console.log(resultado);*/
 
+//desafio3 pedir nombre y apellido y mostrarlo completo
+const nombre = prompt("ingrese su nombre");
+const apellido = prompt("ingrese su apellido");
+resultado = nombre + apellido;
+console.log(resultado);
 
 
 
